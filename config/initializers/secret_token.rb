20852ae76ba34ae540f7c64f85721c08cbf11da4
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2013-01-25 15:54:31 +1100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fate::Application.config.secret_token = 'aa3abe0bd9700c7ace76010683f911a01efd099a4fb0fecc61f93ab2e7f51597d065af47e2d6807ce64e608960ee33ff3cfabb37583d4f4d3eb51bd866fcfe27'
